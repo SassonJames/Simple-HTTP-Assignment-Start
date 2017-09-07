1620626ed82feb7c99750f67d49e6181e0ffdc32
Jamie Sasson <sassonjames@gmail.com>
2017-09-06 22:47:31 -0400

    Removed console logs

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ const imageHandler = require('./imageResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-  console.log(request.url);
   switch (request.url) {
     case '/':
       htmlHandler.getIndex(request, response);
@@ -37,5 +36,3 @@ const onRequest = (request, response) => {
 };
 
 http.createServer(onRequest).listen(port);
-
-console.log(`Listening on 127.0.0.1: ${port}`);
